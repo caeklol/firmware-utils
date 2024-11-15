0d7b0c7d87ae7e76b0ad2991b4b2740719e2220b
naf <naf@sdf.org>
2024-11-15 08:05:51 -0600

    add soft_ver text

--- src/tplink-safeloader.c
+++ src/tplink-safeloader.c
@@ -1774,7 +1774,7 @@ static struct device_info boards[] = {
 			"{product_name:M4R,product_ver:3.0.0,special_id:4B520000}\n"
 			"{product_name:M4R,product_ver:3.0.0,special_id:42340000}\n",
 		.part_trail = 0x00,
-		.soft_ver = SOFT_VER_DEFAULT,
+		.soft_ver = SOFT_VER_TEXT("soft_ver:7.0.0\n"),
 
 		.partitions = {
 			{"SBL1", 0x00000, 0x30000},
